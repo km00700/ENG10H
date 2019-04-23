cd9608e299d657997f2ec7bddbee3fc3d4f522d9
km00700 <km00700@gmail.com>
2019-04-23 09:26:14 -0700

    Implemented numeric gradient descent
    Added delta value to be used in the calculation of the gradient.

---
+++ Homework 2, Kai Mills/numeric_grad.m
@@ -0,0 +1,8 @@
+function f = numeric_grad(v, delta) 
+    f = [0 0];
+    dxi = [delta 0];
+    dyi = [0 delta];
+    dz_dx = (z2_function(v+dxi) - z2_function(v-dxi)) / 2*delta;
+    dz_dy = (z2_function(v+dyi) - z2_function(v-dyi)) / 2*delta;
+    f = [dz_dx, dz_dy];
+end
---
+++ Homework 2, Kai Mills/numeric_minimum.m
@@ -0,0 +1,26 @@
+alpha = 0.0004;
+delta = 0.2;
+stopping_value = 0.01;
+prev_point = [-4, 4];
+current_point = prev_point;
+iterations = 0;
+gradient = [100 100];
+
+
+[X Y] = meshgrid(-8:1:8,-8:1:8);
+Z = ((0.04*X.^4)-(5*X.^2.*Y)+(0.01*Y.^4)-(0.3*X.^3.*Y)+(0.005*Y.^6)-(X.^3))./(0.0003*X.^8 + 0.002*Y + 1);
+surf(X,Y,Z,'EdgeColor','None');
+hold on;
+
+while (norm(gradient) > stopping_value)
+    iterations = iterations + 1;
+    gradient = numeric_grad(current_point, delta);
+    current_point = prev_point - alpha * gradient;
+    plot3(current_point(1), current_point(2), z2_function(current_point),'m*');
+    prev_point = current_point;
+end
+
+
+disp(current_point);
+%disp(gradient);
+disp(iterations);
