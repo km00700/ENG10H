e9d25a80c5555ff90ca99df9055373ed62351cf6
km00700 <km00700@gmail.com>
2019-05-02 23:17:11 -0700

    Made Partial Pathing Universal

--- Homework 3, Kai Mills/Asteroid_search.m
+++ Homework 3, Kai Mills/Asteroid_search.m
@@ -1,17 +1,17 @@
-starting_position = [18, 0];
-maximum_search_depth = 10;
+starting_position = [10, 0];
+course_depth = 20;
+maximum_search_depth = 9;
 maximum_movement_distance = 4;
 asteroid_field = load('asteroids.mat');
 asteroid_field = asteroid_field.ast_coords;
 
 path = starting_position;
 
-for(i = 1:5)
-   [route, depth] = find_path(starting_position, maximum_search_depth, asteroid_field);
+for(i = 1:ceil(course_depth/maximum_movement_distance))
+   route = find_path(starting_position, maximum_search_depth, asteroid_field);
    path = [path; route(2:maximum_movement_distance + 1,:)];
    starting_position = route(maximum_movement_distance + 1,:);
 end
 
-disp(path);
-
-animate_system(path, asteroid_field);
+disp(path(1:21,:));
+animate_system(path(1:21,:), asteroid_field);
--- Homework 3, Kai Mills/find_path.m
+++ Homework 3, Kai Mills/find_path.m
@@ -1,30 +1,35 @@
-function [positions, depth] = find_path(position, max_tree_depth, ast_coords)
+function [positions, depth, counter] = find_path(position, max_tree_depth, ast_coords)
     %Returns current position and previous depth if Millenium Falcon is 
     %colliding with an asteroid.
     if(check_collision(position, ast_coords) >= 1) 
         positions = position;
         depth = position(2) - 1;
+        counter = 1;
     %Returns current position and current depth if Millenium Falcon has
     %reached the maximum search depth.
     elseif(max_tree_depth <= 0)
         positions = position;
         depth = position(2);
+        counter = 1;
     %Returns the postions from the recursive search for the largest depth.
     else
         left_path_length = -1;
         right_path_length = -1;
         center_path_length = 0;
         
-        [center_path, center_depth] = find_path(position + [0, 1], max_tree_depth - 1, ast_coords);
+        left_count = 0;
+        right_count = 0;
+        
+        [center_path, center_depth, center_count] = find_path(position + [0, 1], max_tree_depth - 1, ast_coords);
         center_path_length = center_depth;
        
         if(position(1) > 0)
-            [left_path, left_depth] = find_path(position + [-1, 1], max_tree_depth - 1, ast_coords);
+            [left_path, left_depth, left_count] = find_path(position + [-1, 1], max_tree_depth - 1, ast_coords);
             left_path_length = left_depth;
         end
         
         if(position(1) < 20)
-            [right_path, right_depth] = find_path(position + [1, 1], max_tree_depth - 1, ast_coords);
+            [right_path, right_depth, right_count] = find_path(position + [1, 1], max_tree_depth - 1, ast_coords);
             right_path_length = right_depth;
         end
         
@@ -39,7 +44,8 @@ function [positions, depth] = find_path(position, max_tree_depth, ast_coords)
             longest_depth = left_depth;
         end
         
-        positions = [position; [longest_path(:,1), longest_path(:,2)]];
+        positions = [position; longest_path];
         depth = longest_depth;
+        counter = left_count + right_count + center_count;
     end
 end
