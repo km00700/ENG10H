5e9f0f5a941b640a10e3f8f530f1b015756b50ff
Vivian Ross <vivianross@169-231-106-14.wireless.ucsb.edu>
2019-06-05 15:34:08 -0700

    correct slalom.m

--- Final/the_great_slalom/slalom.m
+++ Final/the_great_slalom/slalom.m
@@ -1,22 +1,4 @@
-function [x, y]=slalom(vx0, vx, x0, y0)
-    vmax=2;
-    vy0=-(vmax^2-vx0^2)^(0.5);
-    vy=-(vmax^2-vx^2)^(0.5);
-    ax=(vx-vx0); %makes acceleration constant
-    ay=(vy-vy0);
-    VX=[x0; vx0];
-    VY=[y0; vy0];
-    tspan=[0:0.1:1]; %delta t is always one second
-    xinitial=[x0; vx0];
-    yinitial=[y0; vy0];
-    [t,X]=ode23(@xvelocity, tspan, xinitial);
-    [t,Y]=ode23(@yvelocity, tspan, yinitial);
-    x=X;
-    y=Y;
-    function xdot=xvelocity(t, VX)
-        xdot=[VX(2,1); ax];
-    end
-    function ydot=yvelocity(t, VY)
-        ydot=[VY(2,1); ay];
-    end
+function derivative=slalom(t,V) %V is column vector with x0, vx0, y0, vy0
+    global ax ay
+    derivative=[V(2,1); ax; V(4,1); ay];
 end
