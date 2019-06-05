efe9c27457c12f165f70232e1f42c2de8ec37883
Vivian Ross <vivianross@169-231-109-49.wireless.ucsb.edu>
2019-06-04 21:44:10 -0700

    less fucked up slalom

--- Final/the_great_slalom/slalom.m
+++ Final/the_great_slalom/slalom.m
@@ -2,17 +2,21 @@ function [x, y]=slalom(vx0, vx, x0, y0)
     vmax=2;
     vy0=-(vmax^2-vx0^2)^(0.5);
     vy=-(vmax^2-vx^2)^(0.5);
+    ax=(vx-vx0); %makes acceleration constant
+    ay=(vy-vy0);
+    VX=[x0; vx0];
+    VY=[y0; vy0];
     tspan=[0:0.1:1]; %delta t is always one second
     xinitial=[x0; vx0];
     yinitial=[y0; vy0];
     [t,X]=ode23(@xvelocity, tspan, xinitial);
     [t,Y]=ode23(@yvelocity, tspan, yinitial);
-    x=X;
-    y=Y;
-    function xdot=xvelocity(vx, t)
-        xdot=[vx; (vx-vx0)];
+    x=X(:,1);
+    y=Y(:,1);
+    function xdot=xvelocity(t, VX)
+        xdot=[VX(2,1); ax];
     end
-    function ydot=yvelocity(vy, t)
-        ydot=[vy; vy-vy0];
+    function ydot=yvelocity(t, VY)
+        ydot=[VY(2,1); ay];
     end
 end
